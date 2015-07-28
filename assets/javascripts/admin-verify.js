e0eb56251fb3bc58013ac62466ab48e3b0919816
Zane <zane@zanematthew.com>
2015-07-28 16:43:21 -0400

    Removing old js

--- assets/javascripts/admin-verify.js
+++ assets/javascripts/admin-verify.js
@@ -8,50 +8,36 @@ jQuery( document ).ready(function( $ ){
             $field_id = '#' + $store_info.field_id,
             $current_value = $( $field_id ).val();
 
-        console.log( $current_value );
+        $this.attr('disabled', true);
 
-        if ( $store_info.length == 0 ){
+        $.ajax({
+            url: ajaxurl,
+            dataType: 'json',
+            type: 'POST',
+            data: {
+                action: 'zmLicenseAjaxValidate',
+                license_action: $action,
+                license_key: $store_info.license,
+                previous_license: $current_value,
+                download_name: $store_info.download,
+                settings_id: $store_info.settings_id,
+                store: $store_info.store
+            },
+            success: function( msg ){
 
-            $('#zm_license_action').val( $action );
-            $this.parents('form').submit();
+                $this.val( msg.button_text );
 
-            return false;
-
-        } else {
-
-            $this.attr('disabled', true);
-
-            $.ajax({
-                url: ajaxurl,
-                dataType: 'json',
-                type: 'POST',
-                data: {
-                    action: 'zmLicenseAjaxValidate',
-                    license_action: $action,
-                    license_key: $store_info.license,
-                    previous_license: $current_value,
-                    download_name: $store_info.download,
-                    settings_id: $store_info.settings_id,
-                    store: $store_info.store
-                    // namespace: $this.data('namespace'),
-                    // _wpnonce: $this.data('quilt_restore_default_nonce')
-                },
-                success: function( msg ){
-                    console.log( msg );
-                    $this.val( msg.button_text );
-
-                    if ( msg.type == 'deactivated' ){
-                        $this.data( 'zm_license_action', 'license_activate' );
-                    }
-
-                    if ( msg.type == 'valid' ){
-                        $this.data( 'zm_license_action', 'license_deactivate' );
-                    }
-
-                    $this.attr('disabled', false);
-
+                if ( msg.type == 'deactivated' ){
+                    $this.data( 'zm_license_action', 'license_activate' );
                 }
-            });
-        }
+
+                if ( msg.type == 'valid' ){
+                    $this.data( 'zm_license_action', 'license_deactivate' );
+                }
+
+                $this.attr('disabled', false);
+
+            }
+        });
     });
 });
